9f3a06889a1eb639a7cf1ef2217b401d10c53cda
Cycle9898 <cycle9898@gmail.com>
2024-03-14 18:43:27 +0100

    added GET test endpoint to fetch data from one sold item

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.get("/api/stuff", (req, res, next) => {
 		.catch(error => res.status(400).json({ error }));
 });
 
+app.get("/api/stuff/:id", (req, res, next) => {
+	Thing.findOne({ _id: req.params.id })
+		.then(thing => res.status(200).json(thing))
+		.catch(error => res.status(404).json({ error }));
+});
+
 app.post("/api/stuff", (req, res, next) => {
 	delete req.body._id;
 
