3912e5001f5228f11e8f0e2ca56acf10160d7a26
Cycle9898 <cycle9898@gmail.com>
2024-03-15 18:05:03 +0100

    deported business logics from stuff Router into stuff controllers

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,40 +1,14 @@
 import express from "express";
-import { Thing } from "../database/models/Thing.js";
+import * as stuffCtrl from "../controllers/stuff.js";
 
 export const stuffRouter = express.Router();
 
-stuffRouter.get("/", (req, res, next) => {
-	Thing.find()
-		.then(things => res.status(200).json(things))
-		.catch(error => res.status(400).json({ error }));
-});
+stuffRouter.get("/", stuffCtrl.getThings);
 
-stuffRouter.get("/:id", (req, res, next) => {
-	Thing.findOne({ _id: req.params.id })
-		.then(thing => res.status(200).json(thing))
-		.catch(error => res.status(404).json({ error }));
-});
+stuffRouter.get("/:id", stuffCtrl.getOneThing);
 
-stuffRouter.post("/", (req, res, next) => {
-	delete req.body._id;
+stuffRouter.post("/", stuffCtrl.createThing);
 
-	const thing = new Thing({
-		...req.body
-	});
-	thing
-		.save()
-		.then(() => res.status(201).json({ message: "Objet enregistré !" }))
-		.catch(error => res.status(400).json({ error }));
-});
+stuffRouter.put("/:id", stuffCtrl.modifyThing);
 
-stuffRouter.put("/:id", (req, res, next) => {
-	Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-		.then(() => res.status(200).json({ message: "Objet modifié !" }))
-		.catch(error => res.status(400).json({ error }));
-});
-
-stuffRouter.delete("/:id", (req, res, next) => {
-	Thing.deleteOne({ _id: req.params.id })
-		.then(() => res.status(200).json({ message: "Objet supprimé !" }))
-		.catch(error => res.status(400).json({ error }));
-});
+stuffRouter.delete("/:id", stuffCtrl.deleteThing);
