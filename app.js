28d319a74c8ee5856c28b2fb8719c82a829eae8e
Cycle9898 <cycle9898@gmail.com>
2024-03-15 16:43:53 +0100

    added DELETE endpoint with database interaction to delete a sold item

--- app.js
+++ app.js
@@ -59,3 +59,9 @@ app.put("/api/stuff/:id", (req, res, next) => {
 		.then(() => res.status(200).json({ message: "Objet modifié !" }))
 		.catch(error => res.status(400).json({ error }));
 });
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+	Thing.deleteOne({ _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Objet supprimé !" }))
+		.catch(error => res.status(400).json({ error }));
+});
