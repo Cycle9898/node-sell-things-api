38a71566f3910b4ae145281449b045854291f6f4
Cycle9898 <cycle9898@gmail.com>
2024-03-18 16:18:44 +0100

    added login controller partial logic

--- controllers/user.js
+++ controllers/user.js
@@ -18,4 +18,32 @@ export function signup(req, res, next) {
 		.catch(error => res.status(500).json({ error }));
 }
 
-export function login(req, res, next) {}
+export function login(req, res, next) {
+	User.findOne({ email: req.body.email })
+		.then(user => {
+			if (!user) {
+				return res
+					.status(401)
+					.json({ message: "Identifiant ou mot de passe incorrect" });
+			}
+
+			bcrypt
+				.compare(req.body.password, user.password)
+				.then(valid => {
+					if (!valid) {
+						return res
+							.status(401)
+							.json({
+								message: "Identifiant ou mot de passe incorrect"
+							});
+					}
+
+					res.status(200).json({
+						userId: user._id,
+						token: "TOKEN"
+					});
+				})
+				.catch(error => res.status(500).json({ error }));
+		})
+		.catch(error => res.status(500).json({ error }));
+}
