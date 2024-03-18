17aedccaded830e4666753589e9989eb8f37b931
Cycle9898 <cycle9898@gmail.com>
2024-03-18 15:56:35 +0100

    added signup controller logic

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,21 @@
-export function signup(req, res, next) {}
+import bcrypt from "bcrypt";
+import { User } from "../database/models/User.js";
+
+export function signup(req, res, next) {
+	bcrypt
+		.hash(req.body.password, 10)
+		.then(hash => {
+			const user = new User({
+				email: req.body.email,
+				password: hash
+			});
+			user.save()
+				.then(() =>
+					res.status(201).json({ message: "Utilisateur créé !" })
+				)
+				.catch(error => res.status(400).json({ error }));
+		})
+		.catch(error => res.status(500).json({ error }));
+}
 
 export function login(req, res, next) {}
