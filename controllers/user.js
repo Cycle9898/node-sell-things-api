b9550d99711870b9c86dd1e4c7ccb47f8cfc5470
Cycle9898 <cycle9898@gmail.com>
2024-03-18 16:40:25 +0100

    handled jwt in login controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { User } from "../database/models/User.js";
 
 export function signup(req, res, next) {
@@ -31,16 +32,19 @@ export function login(req, res, next) {
 				.compare(req.body.password, user.password)
 				.then(valid => {
 					if (!valid) {
-						return res
-							.status(401)
-							.json({
-								message: "Identifiant ou mot de passe incorrect"
-							});
+						return res.status(401).json({
+							message: "Identifiant ou mot de passe incorrect"
+						});
 					}
 
+					const token = jwt.sign(
+						{ userId: user._id },
+						process.env.SECRET_KEY || "DEFAULT_SECRET_KEY",
+						{ expiresIn: "24h" }
+					);
 					res.status(200).json({
 						userId: user._id,
-						token: "TOKEN"
+						token
 					});
 				})
 				.catch(error => res.status(500).json({ error }));
